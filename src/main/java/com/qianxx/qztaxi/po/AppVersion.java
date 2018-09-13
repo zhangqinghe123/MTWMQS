d4eb23d2d8beccfe428139ce1817708f3906a0ba
zhangqinghe123 <qingheemail@126.com>
2018-09-13 17:29:20 +0800

    banben

--- src/main/java/com/qianxx/qztaxi/po/AppVersion.java
+++ src/main/java/com/qianxx/qztaxi/po/AppVersion.java
@@ -1,129 +1,65 @@
 package com.qianxx.qztaxi.po;
 
-import java.io.Serializable;
+import java.util.Date;
 
 /**
  * AppVersion表
  */
-public class AppVersion implements Serializable {
+public class AppVersion {
 
-	private static final long serialVersionUID = 1L;
+    private Integer versionId; // 版本编码
+    private String versionName; // 版本名称
+    private String versionCode; // 版本号
+    private String downLoadUrl; // 下载地址
+    private String introduce; // 版本介绍
+    private Date createTime; // 创建时间
 
-	private Integer versionId; // 版本编码
-	private String versionName; // 版本名称
-	private String versionCode; // 版本号
-	private Integer terminal; // 1用户端，2司机端
-	private Integer versionType; // 版本更新类型（0非强制1强制更新）
-	private Integer state; // 禁用状态（1已启用，2已禁用）
-	private Integer isDel; // 删除状态（1未删除，2已删除）
-	private String downLoadUrl; // 下载地址
-	private String introduce; // 版本介绍
-	private String md5; // MD5校验值
-	private String purpose; // 用途
-	private java.util.Date ctime; // 创建时间
 
-	public Integer getVersionId() {
-		return versionId;
-	}
+    public Integer getVersionId() {
+        return versionId;
+    }
 
-	public AppVersion setVersionId(Integer versionId) {
-		this.versionId = versionId;
-		return this;
-	}
+    public void setVersionId(Integer versionId) {
+        this.versionId = versionId;
+    }
 
-	public String getVersionName() {
-		return versionName;
-	}
+    public String getVersionName() {
+        return versionName;
+    }
 
-	public AppVersion setVersionName(String versionName) {
-		this.versionName = versionName;
-		return this;
-	}
+    public void setVersionName(String versionName) {
+        this.versionName = versionName;
+    }
 
-	public String getVersionCode() {
-		return versionCode;
-	}
+    public String getVersionCode() {
+        return versionCode;
+    }
 
-	public AppVersion setVersionCode(String versionCode) {
-		this.versionCode = versionCode;
-		return this;
-	}
+    public void setVersionCode(String versionCode) {
+        this.versionCode = versionCode;
+    }
 
-	public Integer getTerminal() {
-		return terminal;
-	}
+    public String getDownLoadUrl() {
+        return downLoadUrl;
+    }
 
-	public void setTerminal(Integer terminal) {
-		this.terminal = terminal;
-	}
+    public void setDownLoadUrl(String downLoadUrl) {
+        this.downLoadUrl = downLoadUrl;
+    }
 
-	public Integer getVersionType() {
-		return versionType;
-	}
+    public String getIntroduce() {
+        return introduce;
+    }
 
-	public AppVersion setVersionType(Integer versionType) {
-		this.versionType = versionType;
-		return this;
-	}
+    public void setIntroduce(String introduce) {
+        this.introduce = introduce;
+    }
 
-	public Integer getState() {
-		return state;
-	}
+    public Date getCreateTime() {
+        return createTime;
+    }
 
-	public AppVersion setState(Integer state) {
-		this.state = state;
-		return this;
-	}
-
-	public Integer getIsDel() {
-		return isDel;
-	}
-
-	public void setIsDel(Integer isDel) {
-		this.isDel = isDel;
-	}
-
-	public String getDownLoadUrl() {
-		return downLoadUrl;
-	}
-
-	public AppVersion setDownLoadUrl(String downLoadUrl) {
-		this.downLoadUrl = downLoadUrl;
-		return this;
-	}
-
-	public String getIntroduce() {
-		return introduce;
-	}
-
-	public AppVersion setIntroduce(String introduce) {
-		this.introduce = introduce;
-		return this;
-	}
-
-	public String getMd5() {
-		return md5;
-	}
-
-	public void setMd5(String md5) {
-		this.md5 = md5;
-	}
-
-	public String getPurpose() {
-		return purpose;
-	}
-
-	public void setPurpose(String purpose) {
-		this.purpose = purpose;
-	}
-
-	public java.util.Date getCtime() {
-		return ctime;
-	}
-
-	public AppVersion setCtime(java.util.Date ctime) {
-		this.ctime = ctime;
-		return this;
-	}
-
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
 }
