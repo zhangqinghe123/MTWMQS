d69997a722d1f412a261636a70e8bebeacefaf99
zhangqinghe123 <qingheemail@126.com>
2018-09-20 18:15:32 +0800

    修改数据库配置文件

--- src/main/java/com/qianxx/qztaxi/datebase/ChangDBInterceptor.java
+++ src/main/java/com/qianxx/qztaxi/datebase/ChangDBInterceptor.java
@@ -12,8 +12,7 @@ public class ChangDBInterceptor {
     public Object changeDB(ProceedingJoinPoint pjp) throws Throwable {
         //AOP切点在Service的 包名.类名
         String path = pjp.getTarget().getClass().getName();
-        //如果servcie的 包名.类名 包含 agency，那说明需要切换为mysql数据源
-        if (path.indexOf("user") != -1) {
+        if (path.contains("user")) {
             DataSourceContextHolder.setDbType(DataSourceName.DATASOURCE_USER);
         } else {
             DataSourceContextHolder.setDbType(DataSourceName.DATASOURCE_SERVICE);
